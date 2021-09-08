bd1d0d8495a1568daeb0a58fbf5f259d7019be0c
k0ttjkEEEE <pomogaybo.vv@gmail.com>
2021-09-08 23:09:10 +0300

    hw4

---
+++ src/pro/sky/java/course1/lesson4/Main.java
@@ -0,0 +1,59 @@
+package pro.sky.java.course1.lesson4;
+
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+        task4();
+
+    }
+
+    public static void task1() {
+        int a = 0;
+        while (a < 10) {
+            a = a + 1;
+            System.out.println(a);
+        }
+        int b = 11;
+        while (b > 1) {
+            b = b - 1;
+            System.out.println(b);
+
+        }
+
+
+    }
+
+    public static void task2() {
+        for (int i = 7; i < 31; i = i + 7) {
+            System.out.println("Сегодня пятница " + i + " число, пора сдать отчет");
+        }
+    }
+
+    public static void task3() {
+        int now = 2021;
+        for (int i = 1821; i < 2121; i++) {
+            if (i % 79 == 0)
+                System.out.println("В этот год комета пролетит " + i);
+
+        }
+    }
+
+    public static void task4() {
+        for (int i = 1; i <= 30; i++) {
+            boolean three = i % 3 == 0;
+            boolean five = i % 5 == 0;
+            boolean threeFive = (i % 3 == 0 && i % 5 == 0);
+            if (threeFive) {
+                System.out.println(i + ": ping pong");
+            } else if (three) {
+                System.out.println(i + ": ping");
+            } else if (five) {
+                System.out.println(i + ": pong");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+}
